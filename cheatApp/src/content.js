556a36ba3e24f9f38742ac38ed00d74c2a5ae599
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-29 23:07:52 +0900

    add css styling

--- cheatApp/src/content.js
+++ cheatApp/src/content.js
@@ -1,3 +1,9 @@
+document.getElementById("onClick").addEventListener("click", onclickButton);
+
+function onClickButton() {
+  console.log("fuck you");
+}
+
 // window.alert(
 //   "画面内に小テストが表示された際に自動で解答を表示します。\n 現在の画面には小テストを見つけられなかったため解答ができませんでした。"
 // );
@@ -13,34 +19,34 @@
 
 // document.getElementById("onClick")?.addEventListener("click", onClickButton);
 
-function onClickButton() {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const url = tabs[0]?.url;
+// function onClickButton() {
+//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//     const url = tabs[0]?.url;
 
-    if (url) {
-      chrome.tabs.executeScript(
-        tabs[0].id,
-        {
-          code: `
-        const qtextElement = document.querySelector('.qtext);
-        const qtextValue = qtextElement?.value;
-        qtextValue;
-      `,
-        },
-        (result) => {
-          const qtextValue = result[0];
-          console.log("URL: ", url);
-          console.log("qtext value: ", qtextValue);
+//     if (url) {
+//       chrome.tabs.executeScript(
+//         tabs[0].id,
+//         {
+//           code: `
+//         const qtextElement = document.querySelector('.qtext);
+//         const qtextValue = qtextElement?.value;
+//         qtextValue;
+//       `,
+//         },
+//         (result) => {
+//           const qtextValue = result[0];
+//           console.log("URL: ", url);
+//           console.log("qtext value: ", qtextValue);
 
-          const message = `${qtextValue}`;
+//           const message = `${qtextValue}`;
 
-          // html
-          const outputElement = document.getElementById("ansertBox");
-          outputElement.textContent = message;
-        }
-      );
-    }
-  });
-}
+//           // html
+//           const outputElement = document.getElementById("ansertBox");
+//           outputElement.textContent = message;
+//         }
+//       );
+//     }
+//   });
+// }
 
-document.getElementById("onClick")?.addEventListener("click", onClickButton);
+// document.getElementById("onClick")?.addEventListener("click", onClickButton);
