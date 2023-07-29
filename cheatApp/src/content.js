ede0d544e2169bd1b730db0a0b2444aa395df8df
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-29 18:38:21 +0900

    add js and html link next i want to catch dom contents

--- cheatApp/src/content.js
+++ cheatApp/src/content.js
@@ -1,3 +1,14 @@
-window.alert(
-  "画面内に小テストが表示された際に自動で解答を表示します。\n 現在の画面には小テストを見つけられなかったため解答ができませんでした。"
-);
+// window.alert(
+//   "画面内に小テストが表示された際に自動で解答を表示します。\n 現在の画面には小テストを見つけられなかったため解答ができませんでした。"
+// );
+
+function onClickButton() {
+  const message = "すみません、テストの問題を認識できませんでした。";
+
+  console.log(message);
+
+  const outputElement = document.getElementById("ansertBox");
+  outputElement.textContent = message;
+}
+
+document.getElementById("onClick")?.addEventListener("click", onClickButton);
