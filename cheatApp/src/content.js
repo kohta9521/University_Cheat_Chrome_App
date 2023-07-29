6badc582b9f93dd7b7bd071fb31c1e6df4f2660e
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-29 18:51:27 +0900

    add qtext not get

--- cheatApp/src/content.js
+++ cheatApp/src/content.js
@@ -2,13 +2,45 @@
 //   "画面内に小テストが表示された際に自動で解答を表示します。\n 現在の画面には小テストを見つけられなかったため解答ができませんでした。"
 // );
 
+// function onClickButton() {
+//   const message = "すみません、テストの問題を認識できませんでした。";
+
+//   console.log(message);
+
+//   const outputElement = document.getElementById("ansertBox");
+//   outputElement.textContent = message;
+// }
+
+// document.getElementById("onClick")?.addEventListener("click", onClickButton);
+
 function onClickButton() {
-  const message = "すみません、テストの問題を認識できませんでした。";
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const url = tabs[0]?.url;
 
-  console.log(message);
+    if (url) {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        {
+          code: `
+        const qtextElement = document.querySelector('.qtext);
+        const qtextValue = qtextElement?.value;
+        qtextValue;
+      `,
+        },
+        (result) => {
+          const qtextValue = result[0];
+          console.log("URL: ", url);
+          console.log("qtext value: ", qtextValue);
 
-  const outputElement = document.getElementById("ansertBox");
-  outputElement.textContent = message;
+          const message = `${qtextValue}`;
+
+          // html
+          const outputElement = document.getElementById("ansertBox");
+          outputElement.textContent = message;
+        }
+      );
+    }
+  });
 }
 
 document.getElementById("onClick")?.addEventListener("click", onClickButton);
