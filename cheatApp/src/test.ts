90c170554b4e562bb6a066c7715686492cd98cab
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-30 16:01:13 +0900

    add now tab url not defined

--- cheatApp/src/test.ts
+++ cheatApp/src/test.ts
@@ -1,3 +1,10 @@
 const onClickBtn = () => {
-
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, tabs => {
+    const url = tabs[0].url;
+    alert(url)
+  })
 }
+
+document.getElementById('onClick')?.addEventListener('click', onClickBtn);
+
+
