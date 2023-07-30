a9071afdfd3b3241cecb268656e28bc7e2616f05
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-31 07:15:56 +0900

    add uni

--- cheatApp/src/test.ts
+++ cheatApp/src/test.ts
@@ -1,8 +1,5 @@
 const onClickBtn = () => {
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, tabs => {
-    const url = tabs[0].url;
-    alert(url)
-  })
+  alert("fuck you")
 }
 
 document.getElementById('onClick')?.addEventListener('click', onClickBtn);
